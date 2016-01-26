718c1654aa85f05f12187d82cf79d53744554ca2
mama-lol <mama-lolo@web.de>
2016-01-26 14:03:46 +0100

    resolved error with ComponentStorage

--- src/main/ComponentStorage.java
+++ src/main/ComponentStorage.java
@@ -1,10 +1,8 @@
 package main;
 
 import dataSelectionPanel.DataSelectionPanel;
-import pathGetter.PathPanel;
 
 public class ComponentStorage {
 	public static  MainFrame MAIN_FRAME;
 	public static DataSelectionPanel DATA_SELECTION_PANEL;
-	public static PathPanel PATH_PANEL;
 }
