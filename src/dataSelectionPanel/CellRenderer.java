204b6f1efe31d098b5a822c3edcff68f2f17fe18
mama-lol <mama-lolo@web.de>
2016-02-03 17:32:17 +0100

    Implemented new view mode for the DataSelectionHadler Now showing subfolders appropriatly

--- src/dataSelectionPanel/CellRenderer.java
+++ src/dataSelectionPanel/CellRenderer.java
@@ -2,11 +2,11 @@ package dataSelectionPanel;
 
 //Main content downloaded from StackOverflow
 import java.awt.Component;
-import java.io.File;
 
 import javax.swing.DefaultListCellRenderer;
 import javax.swing.JLabel;
 import javax.swing.JList;
+import javax.swing.ListModel;
 
 /**
  * adapting display
@@ -30,7 +30,17 @@ public class CellRenderer extends DefaultListCellRenderer {
 		// DefaultListCellRenderer always returns a JLabel
 		// super setups up all the defaults
 
-		JLabel label = (JLabel) super.getListCellRendererComponent(list, value.toString().replace(rootPath, ""), index,
+		String localPath = value.toString().replace(rootPath, "");
+		String[] paths = localPath.split("\\\\");
+		// Adding the blanks for the backslashes(not equally as long but in relation to it)
+		String displayPath = new String(new char[paths.length*2]).replace("\0", " ");
+		// adding the blanks for the rootpath (not equally as long but in relation to it)
+		for (int i = 0; i < paths.length - 1; i++) {
+			displayPath = displayPath + new String(new char[paths[i].length()*2]).replace("\0", " ");
+		}
+		// adding the actual name
+		displayPath = displayPath + paths[paths.length-1];
+		JLabel label = (JLabel) super.getListCellRendererComponent(list, displayPath, index,
 				isSelected, cellHasFocus);
 		// "value" is whatever object you put into the list, you can use it
 		// however you want here
@@ -45,5 +55,24 @@ public class CellRenderer extends DefaultListCellRenderer {
 	 */
 	public void updatePath(String newPath) {
 		rootPath = newPath;
+	}
+
+	/**
+	 * 
+	 * @param model
+	 *            Model in which the object is to be searched
+	 * @param value
+	 *            Entry to search
+	 * @return position of the entry, returns -1 if not found
+	 */
+	private int getPosition(ListModel model, Object value) {
+		// searching for entry
+		for (int i = 0; i < model.getSize(); i++) {
+			if (model.getElementAt(i) == value)
+				return i;
+		}
+		// this should definitely not happen!!!
+		return -1;
+
 	}
 }
